5ef801cd9fd76ffac3ae0a51140d79fb562cf3c9
Nemanja Komar <komar@hawaii.edu>
2020-07-06 15:39:50 -1000

    Implement UI and Download URI string assembly
    Fully working prototype

--- main.js
+++ main.js
@@ -6,9 +6,19 @@ var searchParamCollection = {
     resolution: data_resolution,
     quality: data_quality,
     variables: SELECTED_VARIABLES,
-    constraints: [] //{term: "", searchString: "", S2ID: ""}
+    constraints: [], //{term: "", searchString: "", S2ID: ""}
+    timeRange: []
     //Todo: download file format
 };
+
+// List all select2 element IDs and all of the search parameters (term) that will be listed in the dropdown options
+var searchOptions = {
+    params: [
+        {select2ID: "#select2Station", term: "station_name"},
+        {select2ID: "#select2Country", term: "station_country"},
+    ]
+
+}
 $(document).ready(function () {
 
     var today = new Date();
@@ -25,22 +35,24 @@ $(document).ready(function () {
         time_24hr: true,
 
         onChange: function (selectedDates, dateStr, instance) {
-            console.log("dateStr: " + dateStr);
+            // console.log("onChange selectedDates" + selectedDates[0].toISOString());
+            // console.log("dateStr: " + dateStr);
         },
+        onOpen:
+            function (selectedDates, dateStr, instance) {
+                console.log("CALENDAR OPEN");
+                searchParamCollection.timeRange = [];
+                selectedDates.forEach(date => searchParamCollection.timeRange.push(date.toISOString()))
+            },
 
         onClose: function (selectedDates, dateStr, instance) {
-            console.log("onClose " + selectedDates)
+            console.log("onClose selectedDates: ");
+            selectedDates.forEach(function (value, i) {
+                searchParamCollection.timeRange[i] = value.toISOString();
+            });
         }
     }
 
-    // List all select2 element IDs and all of the search parameters (term) that will be listed in the dropdown options
-    var searchOptions = {
-        params: [
-            {select2ID: "#select2Station", term: "station_name"},
-            {select2ID: "#select2Country", term: "station_country"},
-        ]
-
-    }
 
     $(".flatpickr").flatpickr(calendarOptions);
 
@@ -71,8 +83,12 @@ $(document).ready(function () {
 
     populateAllDropdowns();
 
-    function repopulateDropdowns(searchVariables, searchConstraints) {
-        var url = "https://uhslc.soest.hawaii.edu/erddap/tabledap/global_hourly_fast.json?" + searchVariables + "&distinct()" + searchConstraints;
+    function repopulateDropdowns() {
+        var searchVariables = buildSearchVariablesString(searchOptions.params);
+        //build a string of all search variables and their constraints
+        var searchConstraints = buildSearchConstraintsString(searchParamCollection.constraints);
+        var resolutionAndQuality = buildDataTypeString();
+        var url = "https://uhslc.soest.hawaii.edu/erddap/tabledap/" + resolutionAndQuality + ".json?" + searchVariables + "&distinct()" + searchConstraints;
         console.log("URL: " + url);
         console.log("searchVariables: " + searchVariables + " s2ID: " + " searchConstraint: " + searchConstraints);
         $.getJSON(url, function (data) {
@@ -108,19 +124,13 @@ $(document).ready(function () {
 
     // TODO: will need to include data type (daily/hourly), returned data type, and data quality (fast or research)
     // based on the user selection in the checkmark and/or toggle group
-
     function populateAllDropdowns() {
         // build a string of all search variables
-        var stringBuildSP = "";
-        var separator = [',', ''];
-        searchOptions.params.forEach(function (param, i) {
-            if (i == searchOptions.params.length - 1)
-                stringBuildSP += param.term + separator[1];
-            else
-                stringBuildSP += param.term + separator[0];
-        });
+        var stringBuildSP = buildSearchVariablesString(searchOptions.params);
+        var resolutionAndQuality = buildDataTypeString();
+
         // console.log("stringBuildSP "+stringBuildSP);
-        $.getJSON("https://uhslc.soest.hawaii.edu/erddap/tabledap/global_hourly_fast.json?" + stringBuildSP + "&distinct()", function (data) {
+        $.getJSON("https://uhslc.soest.hawaii.edu/erddap/tabledap/" + resolutionAndQuality + ".json?" + stringBuildSP + "&distinct()", function (data) {
             var tempJson = {"results": [{"id": -1, "text": ""}]};
 
             searchOptions.params.forEach(function (param, j) {
@@ -160,16 +170,9 @@ $(document).ready(function () {
                         S2ID: param.select2ID
                     });
 
-                    // build a string of all search variables
-                    var stringBuild = "";
-                    var separator = ['&', ''];
-                    console.log("SELECTED LENGTH " + searchParamCollection.constraints.length);
-                    searchParamCollection.constraints.forEach(function (param, i) {
-                        stringBuild += separator[0] + param.term + "=" + param.searchString + separator[1];
-                    });
                     // repopulate the boxes with the constraints from the previous step
-                    repopulateDropdowns(stringBuildSP, stringBuild);
-                    console.log("stringSearch: " + stringBuild);
+                    repopulateDropdowns();
+
                 });
 
                 // Add on:unselect listener to each dropdown to clear the string
@@ -179,14 +182,9 @@ $(document).ready(function () {
                             searchParamCollection.constraints.splice(i, 1);
                         }
                     });
-                    // build a string of all search variables
-                    var stringBuild = "";
-                    var separator = ['&', ''];
-                    searchParamCollection.constraints.forEach(function (param, i) {
-                        stringBuild += separator[0] + param.term + "=" + param.searchString + separator[1];
-                    });
+
                     // repopulate the boxes with the constraints from the previous step
-                    repopulateDropdowns(stringBuildSP, stringBuild);
+                    repopulateDropdowns();
                 });
 
                 tempJson = {"results": [{"id": -1, "text": ""}]};
@@ -204,6 +202,34 @@ $(document).ready(function () {
     }
 
 });
+
+// Only happens ones
+function buildSearchVariablesString(collection) {
+    var stringBuild = "";
+    var separator = [',', ''];
+    collection.forEach(function (param, i) {
+        if (i == searchOptions.params.length - 1)
+            stringBuild += param.term + separator[1];
+        else
+            stringBuild += param.term + separator[0];
+    });
+    return stringBuild;
+}
+
+// Changes depending on user input
+function buildSearchConstraintsString(collection) {
+    var stringBuild = "";
+    var separator = ['&', ''];
+    collection.forEach(function (param, i) {
+        stringBuild += separator[0] + param.term + "=" + param.searchString + separator[1];
+    });
+    return stringBuild;
+}
+
+// Returns data resolution and quality ERDDAP API compatible string
+function buildDataTypeString() {
+    return "global_" + searchParamCollection.resolution + "_" + searchParamCollection.quality;
+}
 
 function updateUIURL(_data_resolution, _data_quality, _variablesArr, _searchParamsArr) {
     searchParamCollection = {
@@ -236,6 +262,7 @@ function ValidateResolutionSelection() {
             data_resolution = resolution[i].getAttribute("value")
         }
     }
+    searchParamCollection.resolution = data_resolution;
 }
 
 function ValidateQualitySelection() {
@@ -245,6 +272,7 @@ function ValidateQualitySelection() {
             data_quality = quality[i].getAttribute("value")
         }
     }
+    searchParamCollection.quality = data_quality;
 }
 
 function transformJSONtoSelect2(jsonData, column) {
@@ -258,8 +286,35 @@ function transformJSONtoSelect2(jsonData, column) {
     return tempJson;
 }
 
-function updateSelect2Options() {
+function buildTimeRangeString() {
+    var dateRange = "";
 
+    searchParamCollection.timeRange.forEach(function (value, i) {
+        if (i == 0)
+            dateRange += "&time>=" + value;
+        else
+            dateRange += "&time<=" + value;
+    });
+    return dateRange;
+}
+
+function assembleDownloadLink() {
+    var mainURL = "https://uhslc.soest.hawaii.edu/erddap/tabledap/";
+    var dataFormat = $("#fileFormat :selected").text().split("-")[0].trim();
+    console.log("DATA FORMAT: "+dataFormat);
+    var searchVariables = "";
+    var separator = [',', ''];
+    searchParamCollection.variables.forEach(function (param, i) {
+        if (i == searchParamCollection.variables.length - 1)
+            searchVariables += param + separator[1];
+        else
+            searchVariables += param + separator[0];
+    });
+    var searchConstraints = buildSearchConstraintsString(searchParamCollection.constraints);
+    var timeRangeString = buildTimeRangeString();
+    var finalURL = mainURL + buildDataTypeString() + dataFormat + "?" + searchVariables + timeRangeString + searchConstraints + "&distinct()";
+
+    return finalURL;
 }
 
 function pad(num, size) {
@@ -268,7 +323,8 @@ function pad(num, size) {
     return s;
 }
 
-function downloadData(uri) {
+function downloadData() {
+    var uri = assembleDownloadLink();
 
     $.ajax({
         url: uri,
@@ -277,11 +333,10 @@ function downloadData(uri) {
         // dataType: 'json',
         method: "GET"
     });
-    console.log("CALL THIS URI: " + uri);
 
     function success(data) {
-        console.log("DATA: " + data);
-        console.log("DATA.URL: " + data.url);
+        // console.log("DATA: " + data);
+        // console.log("DATA.URL: " + data.url);
         var iframe = document.createElement("iframe");
         iframe.setAttribute("src", uri);
         iframe.setAttribute("style", "display: none");
